1489ad8e5b55efff32fbe9e04a70308ba8508149
CornTortilas <sammydurbas23@gmail.com>
2026-09-17 20:52:53 +0000

    idk

--- 100_HW/HW3_Divisible/base_code/starter.java
+++ 100_HW/HW3_Divisible/base_code/starter.java
@@ -7,9 +7,46 @@
 import java.util.Scanner;
 
 class starter {
+	public static boolean D(int num, int num1){
+		if(num%num1==0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely.");
+		Scanner Input = new Scanner(System.in);
+		int[] ints = new int[2];
+		for(int i=0;i<2;i++){
+			System.out.print("Enter an integer: ");
+			ints[i] = Input.nextInt();
+			Input.nextLine();
+		}
+		int d;
+		for(int i = 0; i < 2; i++){
+			d = 2;
+			if(D(ints[i],d)){
+				System.out.println(ints[i]+" is divisible by " + d);
+			}
+			else{
+				System.out.println(ints[i]+" is not divisible by " + d);
+			}
+			d = 3;
+			if(D(ints[i],d)){
+				System.out.println(ints[i]+" is divisible by " + d);
+			}
+			d = 4;
+			if(D(ints[i],d)){
+				System.out.println(ints[i]+" is divisible by " + d);
+			}
+			d = 5 ;
+			if(D(ints[i],d)){
+				System.out.println(ints[i]+" is divisible by " + d);
+			}
+			if(D(ints[i],3) && D(ints[i],4) && D(ints[i],5)){
+				System.out.println(ints[i] + " is divisible by 3,4, & 5");
+			}
+		}
 	}
 }
